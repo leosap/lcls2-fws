fd282ad09536b0245ab4056729b71749286d6f89
Larry Ruckman <ruckman@slac.stanford.edu>
2017-02-09 11:07:11 -0800

    major reorg

--- firmware/common/AppLib/ruckus.tcl
+++ firmware/common/AppLib/ruckus.tcl
@@ -3,3 +3,6 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load Source Code
 loadSource -dir "$::DIR_PATH/rtl/"
+
+# Load submodules' code and constraints
+loadRuckusTcl "$::DIR_PATH/../DspCoreLib/SysGenCore"
