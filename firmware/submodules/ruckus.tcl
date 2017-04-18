346741ea16337ef4d034505dfdd2f609bd7b36a9
Larry Ruckman <ruckman@slac.stanford.edu>
2017-04-18 16:24:28 -0700

    updating the ruckus.tcl

--- firmware/submodules/ruckus.tcl
+++ firmware/submodules/ruckus.tcl
@@ -5,4 +5,5 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 loadRuckusTcl "$::DIR_PATH/surf"
 loadRuckusTcl "$::DIR_PATH/lcls-timing-core"
 loadRuckusTcl "$::DIR_PATH/amc-carrier-core"
+loadRuckusTcl "$::DIR_PATH/lcls-bpm-core"
 loadRuckusTcl "$::DIR_PATH/../common/$::env(COMMON_FILE)"
